b06ba0aad9adb945a8e46b9d7b569d74736c8c7f
Dan Siegel and Eric Krause <krause197@gmail.com>
2016-06-20 18:58:39 -0700

    added next BDD

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 var leapYear = function(year) {
-  return false;
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 $(document).ready(function() {
